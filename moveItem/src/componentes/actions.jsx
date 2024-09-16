2c334521ee34a60ce510d6c97ff49233d7c3f266
parvez alam <parvezalam336@gmail.com>
2024-09-16 13:04:27 +0530

    moveite run build update

--- moveItem/src/componentes/actions.jsx
+++ moveItem/src/componentes/actions.jsx
@@ -10,9 +10,10 @@ function Actions({move}) {
     return (
         <>
         
-        <div className='flex flex-col text-white gap-3'>
+        <div className='flex flex-col text-white gap-8'>
             <button onClick={move.moveRight} className=' bg-green-500 h-5 w-5 text-center rounded-md'><BsArrowRightShort/></button>
             <button onClick={move.moveLeft} className=' bg-green-500 h-5 w-5 text-center rounded-md'><BsArrowLeftShort/></button>
+ 
         </div>
         </>
     )
